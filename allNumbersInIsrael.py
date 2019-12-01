e226ace855dd9b20d4143b9ac9b52948fd3b2f0a
SammyShehter <samir46@live.ru>
2019-12-01 13:30:54 +0200

    Change 05 to get anoth index. Output file is 1.1GB

---
+++ allNumbersInIsrael.py
@@ -0,0 +1,9 @@
+﻿for h in list(range(10)):
+	for g in list(range(10)):
+		for f in list(range(10)):
+			for e in list(range(10)):
+				for d in list(range(10)):
+					for c in list(range(10)):
+						for b in list(range(10)):
+							for a in list(range(10)):
+								print ("05"+str(a)+str(b)+str(c)+str(d)+str(e)+str(f)+str(g)+str(h))
