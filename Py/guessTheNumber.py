2d7a3a6c96dca99891c03c08ade6e07f7e0152ef
SammyShehter <samir_shehter@yahoo.com>
2020-05-04 19:35:19 +0300

    kek

---
+++ Py/guessTheNumber.py
@@ -0,0 +1,8 @@
+import.random
+theNumber = random.randint(1,20)
+guesses = 0
+print('\'Guess the number\' welcomes you stranger')
+print('Please enter your name: ')
+user = input()
+print('Hello ' + user + '. I\'ve picked a random number in range of 20 and you\'ll have to guess it')
+
