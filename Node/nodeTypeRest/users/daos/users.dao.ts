fd7e9aac389dd90ad0fe1bd1554c7a89e62d5b9f
SammyShehter <samir_shehter@yahoo.com>
2021-02-18 09:11:39 +0200

    updates

--- Node/nodeTypeRest/users/daos/users.dao.ts
+++ Node/nodeTypeRest/users/daos/users.dao.ts
@@ -13,11 +13,54 @@ class UsersDao {
     }
 
     static getInstance(): UsersDao {
-        if(!UsersDao.instance) {
+        if (!UsersDao.instance) {
             UsersDao.instance = new UsersDao()
         }
         return UsersDao.instance
     }
+
+    async addUser(user: UserDto) {
+        user.id = shortid.generate()
+        this.users.push(user)
+        return user.id
+    }
+
+    async getUSers() {
+        return this.users
+    }
+
+    async getUsersById(userId: string) {
+        return this.users.find((user: { id: string }) => user.id === userId)
+    }
+
+    async putUserById(user: UserDto) {
+        const objIndex = this.users.findIndex((obj: { id: string }) => obj.id === user.id)
+        this.users.splice(objIndex, 1, user)
+        return `${user.id} updated via put`
+    }
+
+    async patchById(user: UserDto) {
+        const objIndex = this.users.findIndex((obj: { id: string }) => obj.id === user.id)
+        let currentUser = this.users[objIndex]
+        const allowedPatchFields = ['password', 'firstName', 'lastName', 'premissionLevel']
+        for(let field of allowedPatchFields) {
+            if(field in user) {
+                // @ts-ignore
+                currentUser[field] = user[field]
+            }
+        }
+        this.users.splice(objIndex, 1, currentUser)
+        return `${user.id} patched`
+    }
+
+    async removeUserById(userId: string) {
+        const objIndex = this.users.findIndex((user: { id : string }) => user.id === userId)
+        this.users.splice(objIndex, 1)
+        return `${userId} removed`
+    }
+
+
+
 }
 
 export default UsersDao.getInstance()
