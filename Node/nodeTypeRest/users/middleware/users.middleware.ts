bd76b9639959ea15e9e7209a2e8db63627fc8365
sammyshehter <samir_shehter@yahoo.com>
2021-03-13 23:51:04 +0200

    done!

--- Node/nodeTypeRest/users/middleware/users.middleware.ts
+++ Node/nodeTypeRest/users/middleware/users.middleware.ts
@@ -35,6 +35,20 @@ class UsersMiddleware {
             next()
         }
     }
+
+    async validateUserExists(req: express.Request, res: express.Response, next: express.NextFunction) {
+        const user = await usersService.readById(req.params.userId)
+        if (user) {
+            next()
+        } else {
+            res.status(404).send({error: `User ${req.params.userId} not found`})
+        }
+    }
+
+    async extractUserId(req: express.Request, res: express.Response, next: express.NextFunction) {
+        req.body.id = req.params.userId
+        next()
+    }
 }
 
 export default new UsersMiddleware()
