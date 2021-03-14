b0ba39d3e8a8d4618bc4edefe008b094f477b18a
sammyshehter <sammyshehter@yahoo.com>
2021-03-14 11:46:51 +0200

    will play with this at home

--- Node/nodeTypeRest/app.ts
+++ Node/nodeTypeRest/app.ts
@@ -1,6 +1,5 @@
 import express from 'express'
 import * as http from 'http'
-import * as bodyparser from 'body-parser'
 
 import * as winston from 'winston'
 import * as expressWinston from 'express-winston'
@@ -15,8 +14,6 @@ const server: http.Server = http.createServer(app)
 const port : Number = 3000
 const routes: Array<CommonRoutesConfig> = []
 const debugLog: debug.IDebugger = debug('app')
-
-app.use(bodyparser.json())
 
 app.use(cors())
 
