7971e8274cc2cba6cd954d64de810ddf46c44dfd
sammyshehter <samir_shehter@yahoo.com>
2021-03-31 00:40:10 +0300

    changes in weather app

---
+++ Node/nodeCourse/weather-app/app.js
@@ -0,0 +1,9 @@
+const request = require('postman-request')
+
+const url =
+    'http://api.weatherstack.com/current?access_key=08def42b45ac86611772ffb6ddb783d9&query=32.08227, 34.81065'
+
+request.get({ url, json: true }, (error, response) => {
+    const {temperature, feelslike} = response.body.current
+    console.log(`It's ${temperature} degrees outside, but feels like ${feelslike} degrees`)
+})
